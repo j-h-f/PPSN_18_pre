0887ce150f9cdd61a8b630134c76a7a35e4c5e08
JonasBrother <j.spilles@campus.tu-berlin.de>
2018-04-27 17:40:54 +0200

    Feature
    Button und Impressum hinzugefügt

--- SozialeNetze_vorbereitung/script.js
+++ SozialeNetze_vorbereitung/script.js
@@ -1,10 +1,23 @@
 window.onload = function(){
 	var create = document.getElementById("create_new_btn");
 	create.onclick = function() {new_todo();} 
-
-	var edit = document.getElementById("")
-
+    
+    var impressum = document.getElementById("impressum_btn");
+	impressum.onclick = function() {goto_impressum();} 
+    
+    var edit = document.getElementById("edit_btn");
+	edit.onclick = function() {goto_edit();} 
+    
+    
+    function goto_impressum(){
+        window.location.href = "impressum.html"; 
+    }
 	function new_todo() {
 		window.location.href = "create_new.html"; 
 	}
+    
+    function goto_edit() {
+		window.location.href = "edit_todo.html"; 
+	}
+  
 }
--- SozialeNetze_vorbereitung/script_new.js
+++ SozialeNetze_vorbereitung/script_new.js
@@ -1,7 +1,14 @@
 window.onload = function(){	
 	var save = document.getElementById("todo_save");
 	save.onclick = function() {save_data();}
-	
+    
+    var create = document.getElementById("home");
+	create.onclick = function() {goHome();} 
+    
+    function goHome() {
+		window.location.href = "index.html"; 
+	}
+    
 	function save_data(){
 		var todo_text = document.getElementById("todo_text").value;
 		var todo_deadline = document.getElementById("todo_deadline").value;
