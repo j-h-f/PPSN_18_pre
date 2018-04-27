21022447e7e3c4122b994b697aeb3aa020a6012e
jan-hendrik <jan-hendrik.fabricius@t-online.de>
2018-04-27 14:05:46 +0200

    improved look of the Website and added edit Page for an existing ToDo

---
+++ SozialeNetze_vorbereitung/script.js
@@ -0,0 +1,10 @@
+window.onload = function(){
+	var create = document.getElementById("create_new_btn");
+	create.onclick = function() {new_todo();} 
+
+	var edit = document.getElementById("")
+
+	function new_todo() {
+		window.location.href = "create_new.html"; 
+	}
+}
---
+++ SozialeNetze_vorbereitung/script_new.js
@@ -0,0 +1,14 @@
+window.onload = function(){	
+	var save = document.getElementById("todo_save");
+	save.onclick = function() {save_data();}
+	
+	function save_data(){
+		var todo_text = document.getElementById("todo_text").value;
+		var todo_deadline = document.getElementById("todo_deadline").value;
+		var todo_progress = document.getElementById("todo_progress").value;
+
+		alert( (todo_text + "\n" + todo_deadline + "\n" + todo_progress) );
+
+		window.location.href = "index.html";
+	}
+}
